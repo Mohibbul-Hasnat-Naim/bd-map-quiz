b73a4b9e4bf12dd14ed7500299382d257760e2a2
Mohibbul Hasnat Naim <60913590+Mohibbul-Hasnat-Naim@users.noreply.github.com>
2026-04-23 19:28:10 +0600

    feat: upgrade to new v6.3 engine & world map quiz (#2)
    * New engine in the world map and Asia map is loaded
    
    * Changing UX of the World Map view
    
    * Preloading bar, better UX
    
    * Inject the reloading bar to all pages
    
    * New engine, Structure, Continents
    
    * Initial view has been fixed
    
    * Latest update and maybe the last for now.

---
+++ adapters/continentAdapter.js
@@ -0,0 +1,77 @@
+import {
+    removeItems,
+    aliases
+} from "../config/worldCountries.js";
+
+function normalize(text) {
+    return text
+        .toLowerCase()
+        .replace(/[_-]/g, " ")
+        .replace(/\s+/g, " ")
+        .trim();
+}
+
+export async function buildContinentMap(allowedCountries) {
+    const rawSvg = await fetch("../maps/world.svg")
+        .then(res => res.text());
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(rawSvg, "image/svg+xml");
+
+    const regions = [
+        ...doc.querySelectorAll("path"),
+        ...doc.querySelectorAll("circle")
+    ];
+
+    const grouped = {};
+    const finalItems = [];
+
+    regions.forEach(path => {
+        let name =
+            path.getAttribute("name") ||
+            path.getAttribute("class") ||
+            path.getAttribute("id");
+
+        if (!name) return;
+
+        name = normalize(name);
+
+        if (aliases[name]) {
+            name = aliases[name];
+        }
+
+        if (removeItems.includes(name)) return;
+        if (!allowedCountries.includes(name)) return;
+
+        path.removeAttribute("class");
+        path.removeAttribute("id");
+
+        path.setAttribute("fill", "#7ed957");
+        path.setAttribute("stroke", "#f30707");
+        path.setAttribute("stroke-width", "1.2");
+
+        if (!grouped[name]) {
+            grouped[name] = [];
+            finalItems.push(name);
+        }
+
+        grouped[name].push(path.outerHTML);
+    });
+
+    let result = "";
+
+    Object.keys(grouped).forEach(name => {
+        result += `
+            <g id="${name}" class="map-region">
+                ${grouped[name].join("\n")}
+            </g>
+        `;
+    });
+
+    return {
+        id: "continent",
+        name: "Continent Map",
+        items: finalItems,
+        svg: result
+    };
+}
